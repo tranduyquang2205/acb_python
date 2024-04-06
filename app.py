2b055448dc23ef1434c536362e4668edd0ef7a61
tranduyquang <tranduyquang2205@gmail.com>
2024-04-06 16:45:22 +0700

    update

--- app.py
+++ app.py
@@ -10,17 +10,15 @@ class LoginDetails(BaseModel):
     username: str
     password: str
     account_number: str
-    device_id: str
-    smart_otp_pin: str
 @app.post('/login', tags=["login"])
 def login_api(input: LoginDetails):
-        acb = ACB(input.username, input.password, input.account_number,input.device_id,input.smart_otp_pin)
+        acb = ACB(input.username, input.password, input.account_number)
         result = acb.login()
         return result
 
 @app.post('/get_balance', tags=["get_balance"])
 def get_balance_api(input: LoginDetails):
-        acb = ACB(input.username, input.password, input.account_number,input.device_id,input.smart_otp_pin)
+        acb = ACB(input.username, input.password, input.account_number)
         result = acb.login()
         balance = acb.get_balance()
         return balance
@@ -28,13 +26,11 @@ class Transactions(BaseModel):
     username: str
     password: str
     account_number: str
-    device_id: str
-    smart_otp_pin: str
     rows: int
     
 @app.post('/get_transactions', tags=["get_transactions"])
 def get_transactions_api(input: Transactions):
-        acb = ACB(input.username, input.password, input.account_number,input.device_id,input.smart_otp_pin)
+        acb = ACB(input.username, input.password, input.account_number)
         result = acb.login()
         history = acb.get_transactions(input.account_number, input.rows)
         return history
